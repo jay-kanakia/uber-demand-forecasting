d26a83549e82d0389c94c9c870ae346e460756f2
Jay Kanakia <jay.kanakia26@gmail.com>
2026-05-01 08:32:50 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A completed end to end project deployed on aws - Predicting the demand for the cabs in the nyc region helping driver and company to maximize revenue and reduce idle time.',
+    author='Jay Kanakia',
+    license='',
+)
